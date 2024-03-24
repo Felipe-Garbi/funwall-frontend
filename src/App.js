25c0603dfebc747b40f6ff97ed476e99d500c9cb
felipe_garbi <femorishita@gmail.com>
2024-03-23 22:15:15 -0300

    refactor: ensure project is correct

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import { HomePage } from "./pages/home";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Layout>
+          <HomePage />
+        </Layout>
+      ),
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
   );
 }
 
